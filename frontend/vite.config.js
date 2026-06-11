27e728be2553020d85c98bbbad15ab8275330d8d
FarazPC <faraznqidwai@gmail.com>
2026-06-10 19:38:36 -0700

    Change Vite for Local Deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Intercepts all relative paths starting with /api and proxies them to your backend
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
